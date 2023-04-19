0389ac5864deb6567fa72cb4835d92fa6a038f6c
StavitMaimoni <107947548+StavitMaimoni@users.noreply.github.com>
2023-04-20 01:36:22 +0300

    Update BasicMenu.tsx

--- components/BasicMenu.tsx
+++ components/BasicMenu.tsx
@@ -1,24 +1,56 @@
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-
+import useList from '../hooks/useList';
+import useAuth from '../hooks/useAuth';
+import toast, { Toaster } from 'react-hot-toast';
 
 export default function BasicMenu() {
+    const { user, logout } = useAuth();
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
     const open = Boolean(anchorEl);
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         setAnchorEl(event.currentTarget);
     }
     const router = useRouter();
-
+    const list = useList(user?.uid);
     const handleClose = () => {
         setAnchorEl(null);
     }
+    const toastStyle = {
+        background: 'white',
+        color: 'black',
+        fontWeight: 'bold',
+        fontSize: '16px',
+        padding: '15px',
+        borderRadius: '9999px',
+        maxWidth: '1000px',
+    }
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth < 768);
+        };
+
+        // Set the initial value of `isSmallScreen` on mount
+        setIsSmallScreen(window.innerWidth < 768);
+
+        // Add a resize listener to update `isSmallScreen` on window resize
+        window.addEventListener("resize", handleResize);
+
+        // Clean up the resize listener on unmount
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
 
     return (
         <div className="md:!hidden">
+            {isSmallScreen && <Toaster position="bottom-center" />}
+
             <Button
                 id="basic-button"
                 aria-controls={open ? 'basic-menu' : undefined}
@@ -59,11 +91,27 @@ export default function BasicMenu() {
                     handleClose;
                 }
                 }>New & Popular</MenuItem>
-                <MenuItem onClick={() => {
-                    router.push(`/my-list`);
-                    handleClose;
-                }
-                }>My List</MenuItem>
+                {list.length > 0 && <>
+                    <MenuItem onClick={() => {
+                        router.push(`/my-list`);
+                        handleClose;
+                    }
+                    }>My List</MenuItem>
+                </>}
+                {list.length < 1 && <>
+                    <MenuItem onClick={() => {
+                        toast(
+                            ` Your list is empty. Add your favorite movies and TV shows to create your list.`,
+                            {
+                                duration: 8000,
+                                style: toastStyle,
+                            }
+                        );
+                        handleClose;
+                    }
+                    }>My List</MenuItem>
+                </>}
+
             </Menu>
         </div>
     )
